0657b341843c1d0d3655af3ced0344e6429d4317
Sasha Petrenko <sap625@umsystem.edu>
2023-05-01 20:35:34 -0500

    Implement discretized BNF

--- src/Backus-Naur.jl
+++ src/Backus-Naur.jl
@@ -5,6 +5,10 @@
 This file implements the parsing and generation of statements with the Backus-Naur form.
 """
 
+# -----------------------------------------------------------------------------
+# ABSTRACT TYPES
+# -----------------------------------------------------------------------------
+
 """
 Abstract type for formal grammars.
 """
@@ -23,13 +27,17 @@ const GSymbolSet = Set{GSymbol}
 """
 A production rule is a set of vectors of symbols.
 """
-const ProductionRule = Dict{Vector{GSymbol}}
+const ProductionRule = Set{Vector{GSymbol}}
 # const ProductionRule = Dict{GSymbol, GSymbolSet}
 
 """
 A production rule set is simply a set of production rules.
 """
-const ProductionRuleSet = Set{ProductionRule}
+const ProductionRuleSet = Dict{GSymbol, ProductionRule}
+
+# -----------------------------------------------------------------------------
+# STRUCTS
+# -----------------------------------------------------------------------------
 
 """
 Backus-Naur form grammar.
@@ -50,14 +58,53 @@ struct BNF <: Grammar
     """
     The set of production rules of the grammar.
     """
-    P::Set{ProductionRule}
+    P::ProductionRuleSet
 end
+
+# -----------------------------------------------------------------------------
+# METHODS
+# -----------------------------------------------------------------------------
+
+"""
+Constructor for a Backus-Naur Form grammer with an initial set of non-terminal symbols.
+
+# Arguments
+- `N::GSymbolSet`: an initial set of non-terminal grammar symbols.
+"""
+function BNF(N::GSymbolSet)
+    return BNF(
+        N,
+        GSymbolSet(),
+        ProductionRuleSet(),
+    )
+end
+
+"""
+Default constructor for the Backus-Naur Form.
+"""
+function BNF()
+    return BNF(GSymbolSet())
+end
+
+# -----------------------------------------------------------------------------
+# FUNCTIONS
+# -----------------------------------------------------------------------------
 
 # function incremented_gsymbol(bin::Integer)
 
 # end
 
-function DescretizedBNF(data::RealMatrix, N::Vector{GSymbol} ; bins::Integer=10)
+"""
+Creates a grammer for discretizing a set of symbols into a number of bins.
+
+# Arguments
+- `N::GSymbolSet`: the set of non-terminal grammar symbols to use for binning.
+- `bins::Integer=10`: optional, the granularity/number of bins.
+"""
+function DescretizedBNF(N::GSymbolSet ; bins::Integer=10)
+# function DescretizedBNF(data::RealMatrix, N::GSymbolSet ; bins::Integer=10)
+    # Create an initial BNF
+    # bnf = BNF(N)
     # Initialize the terminal symbol set
     T = GSymbolSet()
     # Initialize the production rule set
@@ -70,6 +117,14 @@ function DescretizedBNF(data::RealMatrix, N::Vector{GSymbol} ; bins::Integer=10)
             new_gsymbol = symb + string(ix)
             # Push a binned symbol to the terminals
             push!(T, new_gsymbol)
+            push!(P[symb], new_gsymbol)
         end
     end
+
+    # Return a constructed BNF struct
+    return BNF(
+        N,
+        T,
+        P,
+    )
 end
