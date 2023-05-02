6c205236e8ab3b42cd56a52d5caadf58ca9ee793
Documenter.jl <documenter@juliadocs.github.io>
2023-05-02 03:31:38 +0000

    build based on 010d0d8

--- dev/examples/experiments/simple_experiment.jl
+++ dev/examples/experiments/simple_experiment.jl
@@ -1,2 +1,5 @@
+# Include/run an experiment that generates a random statement from a discretized Iris dataset grammar
+include(joinpath("..", "..", "..", "..", "scripts", "0_init", "ebnf.jl"))
+
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 
