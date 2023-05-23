632bdf40a0419c516e6dbdd476848affa8977861
Sasha Petrenko <sap625@umsystem.edu>
2023-05-23 09:40:50 -0500

    Add pluto and scripts

---
+++ notebooks/pluto/pluto-drafting.jl
@@ -0,0 +1,44 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 74e9205a-5449-49c6-9b9e-1c9f936c88b6
+using Pkg
+
+# ╔═╡ 33fd324d-9032-43c5-b567-6704ccf9335c
+Pkg.activate(".")
+
+# ╔═╡ bae92503-0e8d-40ee-9026-94b7fd8da8fa
+using OAR
+
+# ╔═╡ 78f1b791-4209-470d-a047-60f2062c1af3
+md"# _Pluto Demo_
+
+Pluto is a notebook environment unique to the Julia programming language, updating code as you write it (with computer magic ✨).
+
+Coming from the IPython Jupyter notebook landscape, some things are familiar and some are different.
+For example, Pluto notebooks are just Julia scripts (*.jl) with special comment markings to support reactive compilation, running, live documentation, and more while still being able to be run normally in the REPL.
+"
+
+# ╔═╡ 35e178e3-64db-4d2e-b4a5-8516048ef76c
+md"
+Lets activate the local environment.
+First, use the Julia `Pkg` package:
+"
+
+# ╔═╡ 4b07c22c-7532-4ae9-8d14-fda2689237cb
+md"Then "
+
+# ╔═╡ dfd5186f-bdb2-447d-9361-7933816e71aa
+
+
+# ╔═╡ Cell order:
+# ╟─78f1b791-4209-470d-a047-60f2062c1af3
+# ╟─35e178e3-64db-4d2e-b4a5-8516048ef76c
+# ╠═74e9205a-5449-49c6-9b9e-1c9f936c88b6
+# ╠═4b07c22c-7532-4ae9-8d14-fda2689237cb
+# ╠═33fd324d-9032-43c5-b567-6704ccf9335c
+# ╠═bae92503-0e8d-40ee-9026-94b7fd8da8fa
+# ╠═dfd5186f-bdb2-447d-9361-7933816e71aa
