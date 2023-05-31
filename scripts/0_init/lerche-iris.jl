f7fef155da461ce6811e932d7768472239895cc7
Sasha Petrenko <sap625@umsystem.edu>
2023-05-31 17:02:32 -0500

    Working iris lerche parsing

--- scripts/0_init/lerche-iris.jl
+++ scripts/0_init/lerche-iris.jl
@@ -1,37 +1,15 @@
 using Revise
 using Lerche
 
-json_grammar = raw"""
-    ?start: value
-
-    ?value: object
-            | array
-            | string
-            | SIGNED_NUMBER      -> number
-            | "true"             -> t
-            | "false"            -> f
-            | "null"             -> null
-
-    array  : "[" [value ("," value)*] "]"
-    object : "{" [pair ("," pair)*] "}"
-    pair   : string ":" value
-
-    string : ESCAPED_STRING
-
-    %import common.ESCAPED_STRING
-    %import common.SIGNED_NUMBER
-    %import common.WS
-
-    %ignore WS
-"""
-
-# _separated{x, sep}: x (sep x)*
-
-# num_list: "[" _separated{NUMBER, ","} "]"
 iris_grammar = raw"""
     ?start: statement
 
-    statement: SL SW PL PW
+    statement: sl sw pl pw
+
+    sl : SL -> iris_symb
+    sw : SW -> iris_symb
+    pl : PL -> iris_symb
+    pw : PW -> iris_symb
 
     SL : /SL[1-9]?[0-9]?/
     SW : /SW[1-9]?[0-9]?/
@@ -42,26 +20,25 @@ iris_grammar = raw"""
     %ignore WS
 """
 
-struct TreeToJson <: Transformer end
+struct GramARTTree <: Transformer end
 
-# @inline_rule string(t::TreeToJson, s) = replace(s[2:end-1],"\\\""=>"\"")
-# @rule  pair(t::TreeToJson,p) = Tuple(p)
+# @rule sl(t::GramARTTree, p) = String(p[1])
+# @rule sw(t::GramARTTree, p) = String(p[1])
+# @rule pl(t::GramARTTree, p) = String(p[1])
+# @rule pw(t::GramARTTree, p) = String(p[1])
+@rule iris_symb(t::GramARTTree, p) = String(p[1])
+@rule statement(t::GramARTTree, p) = Set(p)
 
-@rule statement(t::TreeToJson, p) = Set(p)
-
-# iris_parser = Lark(iris_grammar, parser="lalr", lexer="standard", transformer=TreeToJson());
-iris_parser = Lark(iris_grammar, parser="lalr", lexer="standard", transformer=TreeToJson());
+iris_parser = Lark(iris_grammar, parser="lalr", lexer="standard", transformer=GramARTTree());
 text = raw"SL1 SW3 PL4 PW8"
 
-j = Lerche.parse(iris_parser,text)
+k = Lerche.parse(iris_parser,text)
 # T = [
 #     "SL1", "SL2", "SL3", "SL4", "SL5", "SL6", "SL7", "SL8", "SL9", "SL10",
 #     "SW1", "SW2", "SW3", "SW4", "SW5", "SW6", "SW7", "SW8", "SW9", "SW10",
 #     "PL1", "PL2", "PL3", "PL4", "PL5", "PL6", "PL7", "PL8", "PL9", "PL10",
 #     "PW1", "PW2", "PW3", "PW4", "PW5", "PW6", "PW7", "PW8", "PW9", "PW10",
 # ]
-
-# statement = Statement("SL", )
 
 # IRIS BNF grammar, Meuth dissertation p.48, Table 4.6
 # N = {SL, SW, PL, PW}
@@ -83,23 +60,3 @@ j = Lerche.parse(iris_parser,text)
 # 4. <PW> ::=
 # {PW1 | PW2 | PW3 | PW4 | PW5 |
 # PW6 | PW7 | PW8 | PW9 | PW10}
-
-
-# struct TreeToJson <: Transformer end
-
-# @inline_rule string(t::TreeToJson, s) = replace(s[2:end-1],"\\\""=>"\"")
-
-# @rule  array(t::TreeToJson,a) = Array(a)
-# @rule  pair(t::TreeToJson,p) = Tuple(p)
-# @rule  object(t::TreeToJson,o) = Dict(o)
-# @inline_rule number(t::TreeToJson,n) = Base.parse(Float64,n)
-
-# @rule  null(t::TreeToJson,_) = nothing
-# @rule  t(t::TreeToJson,_) = true
-# @rule  f(t::TreeToJson,_) = false
-
-# json_parser = Lark(json_grammar, parser="lalr", lexer="standard", transformer=TreeToJson());
-
-# text = raw"{\"key\": [\"item0\", \"item1\", 3.14]}"
-
-# j = Lerche.parse(json_parser,text)
