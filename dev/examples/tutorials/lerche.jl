05106ab4db1acd2a531eefef706f2404d94a950b
Documenter.jl <documenter@juliadocs.github.io>
2023-06-12 13:20:23 +0000

    build based on 9f261c7

---
+++ dev/examples/tutorials/lerche.jl
@@ -0,0 +1,44 @@
+# Parsing library
+using Lerche
+
+# Statements in this grammar are simply four nonterminal positions with four bins of terminals.
+iris_grammar = raw"""
+    ?start: statement
+
+    statement: sl sw pl pw
+
+    sl : SL -> iris_symb
+    sw : SW -> iris_symb
+    pl : PL -> iris_symb
+    pw : PW -> iris_symb
+
+    SL : /SL[1-9]?[0-9]?/
+    SW : /SW[1-9]?[0-9]?/
+    PL : /PL[1-9]?[0-9]?/
+    PW : /PW[1-9]?[0-9]?/
+
+    %import common.WS
+    %ignore WS
+"""
+
+# The grammar tree subtypes from a Lerche Transformer
+struct GramARTTree <: Transformer end
+
+# The rules turn the terminals into `OAR` grammar symbols and statements into vectors
+@rule iris_symb(t::GramARTTree, p) = OAR.GSymbol{String}(p[1], true)
+@rule statement(t::GramARTTree, p) = Vector(p)
+
+# Create the Lark parser from the grammar, transformer, and additional settings
+iris_parser = Lark(
+    iris_grammar,
+    parser="lalr",
+    lexer="standard",
+    transformer=GramARTTree()
+)
+
+text = raw"SL1 SW3 PL4 PW8"
+
+k = Lerche.parse(iris_parser, text)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
