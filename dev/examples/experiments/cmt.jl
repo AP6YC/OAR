731d038320ac71e1922740253d0796e2058c6c62
Documenter.jl <documenter@juliadocs.github.io>
2023-07-14 17:09:12 +0000

    build based on 901bbb8

---
+++ dev/examples/experiments/cmt.jl
@@ -0,0 +1,22 @@
+# Import the OAR project module
+using OAR
+
+# Location of the edge attributes file, formatted for Lerch parsing
+edge_file = joinpath("..", "assets", "edge_attributes_lerche.txt")
+
+statements = OAR.get_cmt_statements(edge_file)
+
+grammar = OAR.SPOCFG(statements)
+
+gramart = OAR.GramART(
+    grammar,
+    rho=0.1,
+    terminated=false,
+)
+
+for statement in statements
+    OAR.train!(gramart, statement)
+end
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
