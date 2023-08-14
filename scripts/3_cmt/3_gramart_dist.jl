3d89b242b15c8db7ba5d28ab0542ecaac4750c0b
Sasha Petrenko <sap625@umsystem.edu>
2023-08-14 14:34:49 -0500

    Working cmt dist exp

--- scripts/3_cmt/3_gramart_dist.jl
+++ scripts/3_cmt/3_gramart_dist.jl
@@ -27,104 +27,229 @@ using CSV
 # VARIABLES
 # -----------------------------------------------------------------------------
 
-# N_SWEEP = 9
-N_SWEEP = 100
+N_SWEEP = 9
+# N_SWEEP = 100
 RHO_LB = 0.1
 RHO_UB = 0.9
 
 exp_top = "3_cmt"
 exp_name = @__FILE__
 
-# Input CSV file and data definition
-input_file = OAR.data_dir("cmt", "output_CMT_file.csv")
-data_dict_file = OAR.data_dir("cmt", "cmt_data_dict.csv")
-
-# Input file
-output_dir(args...) = OAR.results_dir("3_cmt", args...)
-mkpath(output_dir())
-output_file = output_dir("cmt-clusters-sweep_rho=$(RHO_LB)-$(N_SWEEP)-$(RHO_UB).csv")
-
 # -----------------------------------------------------------------------------
 # PARSE ARGS
 # -----------------------------------------------------------------------------
 
 # Parse the arguments provided to this script
-pargs = OAR.exp_parse(
+pargs = OAR.dist_exp_parse(
     "$(exp_top)/$(exp_name): hyperparameter sweep of GramART for clustering disease protein statements."
 )
 
+# Development override
+pargs["procs"] = 4
+
+# Start several processes
+if pargs["procs"] > 0
+    addprocs(pargs["procs"], exeflags="--project=.")
+end
+
+# Set the simulation parameters
+sim_params = Dict{String, Any}(
+    "m" => "GramART",
+    "rng_seed" => 1234,
+    "rho" => collect(LinRange(
+        RHO_LB,
+        RHO_UB,
+        N_SWEEP
+    ))
+)
+
+
 # -----------------------------------------------------------------------------
-# PARSE ARGS
+# PARALLEL DEFINITIONS
 # -----------------------------------------------------------------------------
 
-# Load the cmt file
-df = OAR.load_cmt(input_file)
+@everywhere begin
+    # Activate the project in case
+    using Pkg
+    Pkg.activate(".")
 
-# Load the data definition dictionary
-df_dict = OAR.load_cmt_dict(data_dict_file)
+    # Modules
+    using OAR
+
+    # Input CSV file and data definition
+    input_file = OAR.data_dir(
+        "cmt",
+        "output_CMT_file.csv",
+    )
+    data_dict_file = OAR.data_dir(
+        "cmt",
+        "cmt_data_dict.csv",
+    )
+
+    # Point to the sweep results
+    sweep_results_dir(args...) = OAR.results_dir(
+        "3_cmt",
+        "sweep",
+        args...
+    )
+
+    # Make the path
+    mkpath(sweep_results_dir())
+
+    # Load the cmt file
+    df = OAR.load_cmt(input_file)
+
+    # Load the data definition dictionary
+    df_dict = OAR.load_cmt_dict(data_dict_file)
+
+    # Turn the statements into TreeNodes
+    ts = OAR.df_to_trees(df, df_dict)
+
+    # Generate a grammart from the statements
+    grammar = OAR.CMTCFG(ts)
+
+    # Generate a simple subject-predicate-object grammar from the statements
+    opts = Dict()
+    opts["grammar"] = grammar
+
+    # Define the single-parameter function used for pmap
+    local_sim(dict) = OAR.tc_gramart(
+        dict,
+        ts,
+        sweep_results_dir,
+        opts,
+    )
+end
 
 # -----------------------------------------------------------------------------
 # EXPERIMENT
 # -----------------------------------------------------------------------------
 
-# Turn the statements into TreeNodes
-ts = OAR.df_to_trees(df, df_dict)
+# Log the simulation scale
+@info "START: $(dict_list_count(sim_params)) simulations across $(nprocs()) processes."
 
-# Generate a grammart from the statements
-grammar = OAR.CMTCFG(ts)
+# Turn the dictionary of lists into a list of dictionaries
+dicts = dict_list(sim_params)
 
-# Create a linrange of rhos to sweep
-rhos = collect(LinRange(
-    RHO_LB,
-    RHO_UB,
-    N_SWEEP
-))
+# Parallel map the sims
+pmap(local_sim, dicts)
+# progress_pmap(local_sim, dicts)
 
-# Init the cluster assignment matrix
-clusters = zeros(Int, length(ts), N_SWEEP)
+# -----------------------------------------------------------------------------
+# CLEANUP
+# -----------------------------------------------------------------------------
 
-# Iterate over all rhos
-for ix in eachindex(rhos)
-    # Initialize the GramART module
-    gramart = OAR.GramART(
-        grammar,
-        rho=rhos[ix],
-        terminated=false,
-    )
+# Close the workers after simulation
+rmprocs(workers())
 
-    # Process the statements
-    @showprogress for tn in ts
-        OAR.train!(gramart, tn)
-    end
+println("--- Simulation complete ---")
 
-    # Classify and add the cluster label to the assignment matrix
-    for jx in eachindex(ts)
-        clusters[jx, ix] = OAR.classify(
-            gramart,
-            ts[jx],
-            get_bmu=true,
-        )
-    end
-end
 
-# Create a copy of the input dataframe for saving corresponding clusters
-out_df = deepcopy(df)
 
-# Classify and push each statement back into a CSV
-# clusters = Vector{Int}()
-# @showprogress for tn in ts
-#     cluster = OAR.classify(gramart, tn, get_bmu=true)
-#     push!(clusters, cluster)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Input CSV file and data definition
+# input_file = OAR.data_dir("cmt", "output_CMT_file.csv")
+# data_dict_file = OAR.data_dir("cmt", "cmt_data_dict.csv")
+
+# # Input file
+# output_dir(args...) = OAR.results_dir("3_cmt", args...)
+# mkpath(output_dir())
+# output_file = output_dir("cmt-clusters-sweep_rho=$(RHO_LB)-$(N_SWEEP)-$(RHO_UB).csv")
+
+# # -----------------------------------------------------------------------------
+# # PARSE ARGS
+# # -----------------------------------------------------------------------------
+
+# # Parse the arguments provided to this script
+# pargs = OAR.exp_parse(
+#     "$(exp_top)/$(exp_name): hyperparameter sweep of GramART for clustering disease protein statements."
+# )
+
+# # -----------------------------------------------------------------------------
+# # PARSE ARGS
+# # -----------------------------------------------------------------------------
+
+# # Load the cmt file
+# df = OAR.load_cmt(input_file)
+
+# # Load the data definition dictionary
+# df_dict = OAR.load_cmt_dict(data_dict_file)
+
+# # -----------------------------------------------------------------------------
+# # EXPERIMENT
+# # -----------------------------------------------------------------------------
+
+# # Turn the statements into TreeNodes
+# ts = OAR.df_to_trees(df, df_dict)
+
+# # Generate a grammart from the statements
+# grammar = OAR.CMTCFG(ts)
+
+# # Create a linrange of rhos to sweep
+# rhos = collect(LinRange(
+#     RHO_LB,
+#     RHO_UB,
+#     N_SWEEP
+# ))
+
+# # Init the cluster assignment matrix
+# clusters = zeros(Int, length(ts), N_SWEEP)
+
+# # Iterate over all rhos
+# for ix in eachindex(rhos)
+#     # Initialize the GramART module
+#     gramart = OAR.GramART(
+#         grammar,
+#         rho=rhos[ix],
+#         terminated=false,
+#     )
+
+#     # Process the statements
+#     @showprogress for tn in ts
+#         OAR.train!(gramart, tn)
+#     end
+
+#     # Classify and add the cluster label to the assignment matrix
+#     for jx in eachindex(ts)
+#         clusters[jx, ix] = OAR.classify(
+#             gramart,
+#             ts[jx],
+#             get_bmu=true,
+#         )
+#     end
 # end
 
-# Add the vigilance parameter columns to the dataframe
-for ix in eachindex(rhos)
-    print_rho = round(rhos[ix]; digits=1)
-    out_df[!, "rho=$(print_rho)"] = clusters[:, ix]
-end
+# # Create a copy of the input dataframe for saving corresponding clusters
+# out_df = deepcopy(df)
 
-# Add the vector as a column to the output dataframe
-# out_df.cluster = clusters
+# # Classify and push each statement back into a CSV
+# # clusters = Vector{Int}()
+# # @showprogress for tn in ts
+# #     cluster = OAR.classify(gramart, tn, get_bmu=true)
+# #     push!(clusters, cluster)
+# # end
 
-# Save the clustered statements to a CSV file
-CSV.write(output_file, out_df)
+# # Add the vigilance parameter columns to the dataframe
+# for ix in eachindex(rhos)
+#     print_rho = round(rhos[ix]; digits=1)
+#     out_df[!, "rho=$(print_rho)"] = clusters[:, ix]
+# end
+
+# # Add the vector as a column to the output dataframe
+# # out_df.cluster = clusters
+
+# # Save the clustered statements to a CSV file
+# CSV.write(output_file, out_df)
